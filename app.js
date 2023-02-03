5d2c32d126a5a26d392abdb9b7734c42f2eede4a
AznRaven <versatilias@gmail.com>
2023-02-02 21:35:21 -0500

    created layout

---
+++ app.js
@@ -0,0 +1,16 @@
+var p1p = document.querySelector('.p1p')
+var p2p = document.querySelector('.p2p')
+var log1 = document.querySelector('.log1')
+var log2 = document.querySelector('.log2')
+var win = document.querySelector('.winner')
+var container = document.querySelector('.container')
+
+
+
+
+function startGame() {
+    // log2.innerHTML = 'hello'
+    document.querySelector('.winner').innerHTML = "Level 1"
+}
+
+startGame()
