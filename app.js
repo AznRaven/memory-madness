d0dfcc409b0bd33c2a3e646d5826c82f7e2fd3f3
AznRaven <versatilias@gmail.com>
2023-02-02 22:09:04 -0500

    get random pattern

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ var log1 = document.querySelector('.log1')
 var log2 = document.querySelector('.log2')
 var win = document.querySelector('.winner')
 var container = document.querySelector('.container')
+var pattern = []
+var copyPattern = []
 
 
 
 
 function startGame() {
     // log2.innerHTML = 'hello'
-    document.querySelector('.winner').innerHTML = "Level 1"
+    win.innerHTML = "Level 1"
+    if(win.innerHTML == 'Level 1'){
+        let count = 0
+        while(count<5){
+            pattern[count] = index = Math.floor(Math.random() * 9)
+            document.querySelector(`n${counter}`).style.backgroundColor = "red"
+            count++
+        }
+    }
+    pattern.forEach(x => log1.innerHTML += x)  
+    
 }
 
+container.addEventListener('click', match)
+
+function match(e) {
+
+}
 startGame()
