326345bb7888b16cdb919216e2cd840617cb96c8
AznRaven <versatilias@gmail.com>
2023-02-03 21:37:18 -0500

    added audio

--- app.js
+++ app.js
@@ -14,15 +14,17 @@ var pattern = []
 var copyPattern = []
 var playerTurn = 2
 var round = 2
-
-
+var waitSound = new soundOnly("b2.mp3")
+var waitSound2 = new soundOnly("go.mp3")
+waitSound.play()
+waitSound2.play()
 function startGame() {
     // log2.innerHTML = 'helloz'
-    if(playerTurn == 2){
+    if (playerTurn == 2) {
         playerTurn = 1
         p1D.style.borderColor = "green"
         p2D.style.borderColor = "white"
-    }else{
+    } else {
         playerTurn = 2
         p2D.style.borderColor = "green"
         p1D.style.borderColor = "white"
@@ -37,13 +39,13 @@ function startGame() {
         async function playPattern() {
             // container.children[0].style.backgroundColor = "white"
             let count = 0
-            if(p1points >= 3 || p2points >= 3){
+            if (p1points >= 3 || p2points >= 3) {
                 win.innerHTML = "Level 2"
                 round = 3
-            }else if(p1points >= 10 || p2points >= 10){
+            } else if (p1points >= 10 || p2points >= 10) {
                 win.innerHTML = "Level 3"
                 round = 5
-            }else if(p1points >= 15 || p2points >= 15){
+            } else if (p1points >= 15 || p2points >= 15) {
                 win.innerHTML = "Level 4"
                 round = 6
             }
@@ -57,25 +59,30 @@ function startGame() {
                 log1.innerHTML += index
                 count++
             }
-            await sleep(500);
+            await sleep(400);
+            s2.play()
+            go.innerHTML = "Ready"
             go.style.backgroundColor = "red"
-            await sleep(500);
+            await sleep(400);
             go.style.backgroundColor = "white"
-            await sleep(500);
+            await sleep(400);
+            s2.play()
+            go.innerHTML = "Set"
             go.style.backgroundColor = "yellow"
-            await sleep(500);
+            await sleep(400);
             go.style.backgroundColor = "white"
-            await sleep(500);
-            go.style.backgroundColor = "green"
-            await sleep(500);
-            go.style.backgroundColor = "white"
-            await sleep(500);
+            await sleep(400);
+            s3.play()
             go.innerHTML = 'GO'
+            go.style.backgroundColor = "green"
+            await sleep(400);
+            go.innerHTML = ''
+            go.style.backgroundColor = "white"
             await sleep(5000);
             await checkPattern()
             startGame()
         }
-        
+
     }
 
     // pattern.forEach(x => log2.innerHTML += x)
@@ -117,22 +124,49 @@ function match(e) {
     }
 }
 
-function checkPattern(){
+function checkPattern() {
     points = 0
     if (copyPattern.length == pattern.length) {
         for (let i = 0; i < pattern.length; i++) {
             pattern[i] == copyPattern[i] ? points += 1 : win.innerHTML = 'bad'
         }
     }
-    if(playerTurn == 1){
+    if (playerTurn == 1) {
         p1points += points
         p1p.innerHTML = p1points
-    }else{
+    } else {
         p2points += points
         p2p.innerHTML = p2points
     }
-    
-    
+
+
 }
 
-startGame()
+function soundOnly(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    // this.sound.volume = .1
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+    this.stop = function () {
+        this.sound.pause();
+    }
+}
+// var s1 = document.getElementById(".s1");
+const s3 = new Audio('go.wav');
+const s2 = new Audio('beep.mp3');
+const s1 = new Audio('bg1.mp3');
+// audioElement.play();
+var btn = document.querySelector('button')
+btn.addEventListener('click', start)
+function start() {
+    s1.volume = .5
+    s1.play()
+    // waitSound2.play()
+    startGame()
+}
